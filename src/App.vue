40c5b9ad15d4427453d97a73d3d5343371ed2113
gianmarcocilia <gianmarcocilia97@gmail.com>
2023-12-04 11:34:36 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import { store } from './store';
+
+export default {
+  data() {
+    return {
+      store,
+    }
+  }
+}
+</script>
+
+<template>
+<h1>New project</h1>
+<span><i class="fa-solid fa-check"></i></span>
+</template>
+
+<style lang="scss">
+@use './style/general.scss';
+@import '@fortawesome/fontawesome-free/css/all.css';
+
+</style>
