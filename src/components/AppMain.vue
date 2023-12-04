ac8be9f4bb06a43d239cc32a7650f11eb2633caf
gianmarcocilia <gianmarcocilia97@gmail.com>
2023-12-04 18:06:58 +0100

    add: section with video, counter and services

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,4 +1,6 @@
 <script>
+import AppButton from './AppButton.vue';
+
 
 export default {
     data() {
@@ -20,10 +22,11 @@ export default {
                     num: 94,
                     thing: 'Countries Served'
                 },
-            ]
+            ],
+
         };
     },
-    
+    components: { AppButton }
 }
 </script>
 
@@ -35,7 +38,9 @@ export default {
                 <div class="section-top text-center">
                     <p class="name">introduction video</p>
                     <h2>Advisory <span>Performance</span></h2>
-                    <p class="paragraph w-50">Financial executives need to review their strategies, operating models and capabilities so that their areas can generate sound information for better decision making.</p>
+                    <p class="paragraph w-50 m-auto pb-5">Financial executives need to review their strategies, operating
+                        models and
+                        capabilities so that their areas can generate sound information for better decision making.</p>
                 </div>
                 <img class="w-75 m-auto" src="../assets/img/video-1.jpg" alt="">
             </div>
@@ -47,11 +52,89 @@ export default {
                 <div class="section-top text-center text-white">
                     <p class="name">what are we doing</p>
                     <h2><span>Results</span> in Numbers</h2>
-                    <div class="section-bottom d-flex">
+                    <div class="section-bottom d-flex justify-content-center">
                         <ul class="m-0 fw-bolder" v-for="counter in counters">
                             <li class="fs-1">{{ counter.num }}</li>
                             <li class="fs-3">{{ counter.thing }}</li>
                         </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Section service -->
+        <section class="section-service">
+            <div class="container">
+                <div class="section-top text-white d-flex align-items-end justify-content-between">
+                    <div class="text w-75">
+                        <p class="name">our business areas</p>
+                        <h2>Excellence in <span>Services</span></h2>
+                        <p class="paragraph m-0">We are leaders in providing consultancy services with a set of cutting-edge
+                            technologies and a team of experienced and renowned professionals. These sre some options that
+                            you
+                            can hire.</p>
+                    </div>
+                    <AppButton text="see all" :isOutline="true" />
+                </div>
+                <div class="section-bottom pt-5">
+                    <div class="container">
+                        <div class="row g-5">
+                            <div class="col-4">
+                                <div class="card bg-dark text-white p-5">
+                                    <h3><i class="fa-solid fa-network-wired"></i></h3>
+                                    <h2>Audit & Assurance</h2>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod inventore ex amet
+                                        placeat, porro aspernatur quibusdam? Ratione, modi nihil fugiat saepe iusto quaerat
+                                        possimus odio tempore. Maiores vitae dolorum mollitia!</p>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="card bg-dark text-white p-5">
+                                    <h3><i class="fa-solid fa-briefcase"></i></h3>
+                                    <h2>Financial Advisory</h2>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod inventore ex amet
+                                        placeat, porro aspernatur quibusdam? Ratione, modi nihil fugiat saepe iusto quaerat
+                                        possimus odio tempore. Maiores vitae dolorum mollitia!</p>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="card bg-dark text-white p-5">
+                                    <h3><i class="fa-solid fa-chart-simple"></i></h3>
+                                    <h2>Analytics and M&A</h2>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod inventore ex amet
+                                        placeat, porro aspernatur quibusdam? Ratione, modi nihil fugiat saepe iusto quaerat
+                                        possimus odio tempore. Maiores vitae dolorum mollitia!</p>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="card bg-dark text-white p-5">
+                                    <h3><i class="fa-solid fa-plane"></i></h3>
+                                    <h2>Middle Marketing</h2>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod inventore ex amet
+                                        placeat, porro aspernatur quibusdam? Ratione, modi nihil fugiat saepe iusto quaerat
+                                        possimus odio tempore. Maiores vitae dolorum mollitia!</p>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="card bg-dark text-white p-5">
+                                    <h3><i class="fa-solid fa-scale-balanced"></i></h3>
+                                    <h2>Legal Consulting</h2>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod inventore ex amet
+                                        placeat, porro aspernatur quibusdam? Ratione, modi nihil fugiat saepe iusto quaerat
+                                        possimus odio tempore. Maiores vitae dolorum mollitia!</p>
+                                </div>
+                            </div>
+                            <div class="col-4">
+                                <div class="card bg-dark text-white p-5">
+                                    <h3><i class="fa-solid fa-inbox"></i></h3>
+                                    <h2>Regulatory Risk</h2>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod inventore ex amet
+                                        placeat, porro aspernatur quibusdam? Ratione, modi nihil fugiat saepe iusto quaerat
+                                        possimus odio tempore. Maiores vitae dolorum mollitia!</p>
+                                </div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
@@ -66,6 +149,7 @@ export default {
 .section-video {
     h2 {
         color: $primary_text_color;
+
         span {
             color: $secondary_text_color;
         }
@@ -73,17 +157,50 @@ export default {
 }
 
 .section-counter {
-    background-image: 
-    linear-gradient(black, rgb(0, 0, 0)),
-    url('../assets/img/bg-route.png');
+    background-image:
+        linear-gradient(black, rgb(0, 0, 0)),
+        url('../assets/img/bg-route.png');
     background-repeat: no-repeat;
     background-size: cover;
     background-blend-mode: saturation;
+
     .section-bottom {
         padding-top: 3rem;
+
         li:first-child {
             color: $secondary_text_color;
         }
+    }
+}
+
+.section-service {
+    background-color: $main_bg_color;
+    text-align: left;
+
+    .section-bottom {
+        h3 {
+            color: $secondary_text_color;
+            font-size: 50px;
+        }
+
+        p {
+            color: $info_color;
+        }
+
+        .card {
+            position: relative;
+
+            &::after {
+                font-family: "Font Awesome 5 Free";
+                font-weight: 900;
+                content: '\f061';
+                position: absolute;
+                right: 10%;
+                font-size: 30px;
+                color: $secondary_text_color;
+            }
+        }
+
     }
 }
 
@@ -91,20 +208,19 @@ section {
     padding: 6rem 0;
 
     .section-top {
-        @include flex(column, center, center);
-        gap: 2rem;
-        text-align: inherit;
 
         .name {
             color: $secondary_text_color;
             text-transform: uppercase;
             font-size: 14px;
             font-weight: 700;
+            padding-bottom: 2rem;
         }
 
         h2 {
             font-size: 40px;
             font-weight: 900;
+            padding-bottom: 2rem;
 
             span {
                 padding: .3em;
@@ -115,7 +231,6 @@ section {
 
         .paragraph {
             color: $info_color;
-            margin-bottom: 2.5rem;
         }
     }
 
@@ -123,4 +238,5 @@ section {
         border-radius: 5px;
         display: block;
     }
-}</style>
+}
+</style>
